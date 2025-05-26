18bf1b50894d7cb26b8ba9b309661b4d353fa19d
Drakenvei <nicholaskensurya@gmail.com>
2025-05-26 21:01:58 +0700

    laporan penjualan owner

--- routes/web.php
+++ routes/web.php
@@ -117,9 +117,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('owner-riwayat-kasir');
 
     Route::get('owner-riwayat-transaksi', [OwnerTransaksiController::class, 'index'])->name('owner-riwayat-transaksi');
-    Route::get('owner-laporan-penjualan', function () {
-        return Inertia::render('owner/owner-laporan-penjualan');
-    })->name('owner-laporan-penjualan');
+    Route::get('owner-laporan-penjualan', [ownerDashboard::class, 'laporanPenjualan'])->name('owner-laporan-penjualan');
+
 });
 
 require __DIR__.'/settings.php';
